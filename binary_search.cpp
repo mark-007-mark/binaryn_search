7400f454cf090a6e49b3343a3927041f6e2a80c3
mark-007-mark <67867849+mark-007-mark@users.noreply.github.com>
2020-10-01 17:56:24 +0530

    Add files via upload

---
+++ binary_search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int Bubble_Sort(int *input,int size,int x){
+    int start=0;
+    int end=size-1;
+    int mid=start;
+    while(start<=end){
+        mid=start+(end-start)/2;
+        if(input[mid]==x){
+            return mid;
+        }else if(input[mid]<x){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n,val;
+    cin>>n>>val;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int ans=Bubble_Sort(arr,n,val);
+    cout<<ans<<endl;
+}
